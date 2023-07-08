08372b100a0decaabb8a496de8951eb6dd1af813
Costa Joseff <rielisson.joseff.costa@ccc.ufcg.edu.br>
2023-07-08 15:41:09 -0300

    Correção de bugs | Adição de funcionalidades na pagina 'Mensagens' | Adição de funcionalidades na pagina index\contato

--- JS/contato.js
+++ JS/contato.js
@@ -1,26 +1,86 @@
-let mensagens = [];
-mensagens = JSON.parse(localStorage.getItem("mensagens"));
-if (mensagens == null) {
-  mensagens = [];
+function coletarArmazenamentoLocal() {
+  let armazenamentoLocal = JSON.parse(localStorage.getItem("mensagens"));
+  if (armazenamentoLocal === null || armazenamentoLocal === undefined || armazenamentoLocal.length == 0) {
+    armazenamentoLocal = [];
+  }
+  return armazenamentoLocal;
 }
 
-let msgID = localStorage.getItem("idDisponivel");
-if (msgID == null) {
-  msgID = 0;
+function coletarMensagemID() {
+  let msgID = localStorage.getItem("idDisponivel");
+  if (msgID === null || msgID === undefined) {
+    msgID = 0;
+  }
+  return msgID;
 }
 
-let conteudo = {};
+function conteudoVazio(conteudo) {
+  return stringVazia(conteudo.nome) || stringVazia(conteudo.email) || stringVazia(conteudo.mensagem);
+}
+
+function limparBotao() {
+  let campoNome = document.getElementById("user-nome");
+  let campoEmail = document.getElementById("user-email");
+  let campoMensagem = document.getElementById("user-msg");
+
+  campoNome.value = "";
+  campoEmail.value = "";
+  campoMensagem.value = "";
+}
+
+function stringVazia(string) {
+  return string.trim().length === 0;
+}
 
 const botao = document.getElementById("botao-enviar");
 botao.addEventListener("click", function coletarMensagem() {
+  let armazenamentoLocal = coletarArmazenamentoLocal();
+  let msgID = coletarMensagemID();
+  let conteudo = {};
+
   conteudo.nome = document.getElementById("user-nome").value;
   conteudo.email = document.getElementById("user-email").value;
   conteudo.mensagem = document.getElementById("user-msg").value;
   conteudo.msgID = msgID;
 
-  mensagens.push(conteudo);
+  if(conteudoVazio(conteudo)) {
+    console.log("Conteúdo do formuládio não pode estar vazio.");
+    return null;
+  }
 
-  localStorage.setItem("mensagens", JSON.stringify(mensagens));
-  mensagens = localStorage.getItem("mensagens");
+  armazenamentoLocal.push(conteudo);
+
+  localStorage.setItem("mensagens", JSON.stringify(armazenamentoLocal));
   localStorage.setItem("idDisponivel", msgID++);
+  limparBotao();
+});
+
+const areaNome = document.getElementById("user-nome");
+areaNome.addEventListener("focusout", function estaVazio() {
+  const nome = document.getElementById("user-nome").value;
+  if (stringVazia(nome)) {
+    areaNome.classList.add("erro");
+  } else {
+    areaNome.classList.remove("erro");
+  }
+});
+
+const areaEmail = document.getElementById("user-email");
+areaEmail.addEventListener("focusout", function estaVazio() {
+  const email = document.getElementById("user-email").value;
+  if (stringVazia(email)) {
+    areaEmail.classList.add("erro");
+  } else {
+    areaEmail.classList.remove("erro");
+  }
+});
+
+const areaMensagem = document.getElementById("user-msg");
+areaMensagem.addEventListener("focusout", function estaVazio() {
+  const mensagem = document.getElementById("user-msg").value;
+  if (stringVazia(mensagem)) {
+    areaMensagem.classList.add("erro");
+  } else {
+    areaMensagem.classList.remove("erro");
+  }
 });
--- JS/mensagens.js
+++ JS/mensagens.js
@@ -1,43 +1,3 @@
-let mensagens = JSON.parse(localStorage.getItem("mensagens"));
-let msgArea = document.getElementById("msg-area");
-for (let i = 0; i < mensagens.length; i++) {
-  let divCard = document.createElement("div");
-  divCard.classList = "card mrg-td-p sombra bg-claro"
-
-  let nome = document.createElement("h2");
-  nome.classList = "d-inline";
-  nome.textContent = mensagens[i].nome;
-
-  let iconLixo = document.createElement("i");
-  iconLixo.classList = "bi bi-trash scale-anim-icon";
-  iconLixo.addEventListener("click", criarIconEventList(iconLixo, mensagens[i].msgID));
-
-  let divNomeIcons = document.createElement("div");
-  divNomeIcons.classList = "d-flex justify-content-between";
-  divNomeIcons.appendChild(nome);
-  divNomeIcons.appendChild(iconLixo);
-
-  let email = document.createElement("p");
-  email.classList = "";
-  email.textContent = mensagens[i].email;
-
-  let hr = document.createElement("hr");
-  
-  let mensagem = document.createElement("p");
-  mensagem.classList = "";
-  mensagem.textContent = mensagens[i].mensagem;
-
-  let divCardBody = document.createElement("div");
-  divCardBody.classList = "inside-box-m";
-
-  divCardBody.appendChild(divNomeIcons);
-  divCardBody.appendChild(email);
-  divCardBody.appendChild(hr);
-  divCardBody.appendChild(mensagem);
-  divCard.appendChild(divCardBody);
-  msgArea.appendChild(divCard);
-}
-
 function criarIconEventList(iconLixo, ID) {
   return function deletarMensagem () {
     let divMsgArea = document.getElementById("msg-area");
@@ -56,4 +16,69 @@ function criarIconEventList(iconLixo, ID) {
       }
     }
   }
+}
+
+function prepararElementos(i) {
+  let divCard = document.createElement("div");
+  divCard.classList = "card mrg-td-p sombra bg-claro"
+
+  let nome = document.createElement("h2");
+  nome.classList = "d-inline";
+  nome.textContent = mensagens[i].nome;
+
+  let iconLixo = document.createElement("i");
+  iconLixo.classList = "bi bi-trash scale-anim-icon pointer altura-icon-lixo";
+  iconLixo.addEventListener("click", criarIconEventList(iconLixo, mensagens[i].msgID));
+
+  let divNomeIcons = document.createElement("div");
+  divNomeIcons.classList = "d-flex justify-content-between";
+  divNomeIcons.appendChild(nome);
+  divNomeIcons.appendChild(iconLixo);
+
+  let email = document.createElement("p");
+  email.classList = "";
+  email.textContent = mensagens[i].email;
+
+  let hr = document.createElement("hr");
+  
+  let mensagem = document.createElement("p");
+  mensagem.classList = "";
+  mensagem.textContent = mensagens[i].mensagem;
+
+  let divCardBody = document.createElement("div");
+  divCardBody.classList = "inside-box-p";
+
+  return {
+    "divCard" : divCard,
+    "divNomeIcons" : divNomeIcons,
+    "email" : email,
+    "hr": hr,
+    "mensagem" : mensagem,
+    "divCardBody": divCardBody
+  };
+}
+
+function appendElements(msgArea, elementos) {
+  elementos.divCardBody.appendChild(elementos.divNomeIcons);
+  elementos.divCardBody.appendChild(elementos.email);
+  elementos.divCardBody.appendChild(elementos.hr);
+  elementos.divCardBody.appendChild(elementos.mensagem);
+  elementos.divCard.appendChild(elementos.divCardBody);
+  msgArea.appendChild(elementos.divCard);
+  return msgArea;
+}
+
+function deletarTudo() {
+  mensagens = [];
+  localStorage.setItem("mensagens", JSON.stringify(mensagens));
+
+  let divMsgArea = document.getElementById("msg-area");
+  divMsgArea.innerHTML = '';
+}
+
+let mensagens = JSON.parse(localStorage.getItem("mensagens"));
+let msgArea = document.getElementById("msg-area");
+for (let i = 0; i < mensagens.length; i++) {
+  const elementos = prepararElementos(i);  
+  msgArea = appendElements(msgArea, elementos);
 }
